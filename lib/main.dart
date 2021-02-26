b5463c98e42c9919d6588bf01abdc21af4f59383
nidhisethi88 <71824113+nidhisethi88@users.noreply.github.com>
2021-02-26 17:04:39 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('images/icon.jpg'),
+              ),
+              Text(
+                'Welcome to Ojo',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'YOUR HELPING HAND',
+                style: TextStyle(
+                  fontFamily: 'Bevan',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              // Card(
+              //     margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              //     child: ListTile(
+              //       leading: Icon(
+              //         Icons.phone,
+              //         color: Colors.teal,
+              //       ),
+              //       title: Text(
+              //         'OCR   Code  Reader',
+              //         style: TextStyle(
+              //           color: Colors.teal.shade900,
+              //           fontFamily: 'Bevan',
+              //           fontSize: 20.0,
+              //         ),
+              //       ),
+              //     )),
+              FlatButton(
+                onPressed: () {
+                  print('Hey!');
+                },
+                child: Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'Object  Detector',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Bevan'),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
